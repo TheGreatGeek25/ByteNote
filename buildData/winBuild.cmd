1f75cd5d88659441651d70ff5037002702766f6d
TheGreatGeek25 <TheGreatGeek25@users.noreply.github.com>
2017-04-21 19:53:56 -0500

    Remove buildData/winBuild.cmd

--- buildData/winBuild.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-SETLOCAL
-set version=%1
-@echo on
-javapackager -deploy -v -native image -BsystemWide=false -Bicon=logos\logoAll.ico -BappVersion=%version% -outdir Windows\C57note64%version%Bundle -outfile C57note64 -srcdir jars -srcfiles C57note64%version%.jar -appclass c57note64.C57note64Main -name C57note64 -title "C57note64"
-@echo off
-copy LICENSE.txt Windows\C57note64%version%Bundle\bundles\C57note64\LICENSE.txt
-ENDLOCAL
