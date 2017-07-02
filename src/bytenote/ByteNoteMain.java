a990af7d8a81c10bc233deb95d6937aeb2f70174
TheGreatGeek25 <TheGreatGeek25@users.noreply.github.com>
2017-07-02 16:50:55 -0500

    Update build.xml and bytenote.ByteNoteMain

--- src/bytenote/ByteNoteMain.java
+++ src/bytenote/ByteNoteMain.java
@@ -12,7 +12,7 @@ public class ByteNoteMain {
 	public static boolean isSaved = true;
 	
 	public static final String name = "ByteNote";
-	public static final String version = "v2.0.4-alpha";
+	public static final String version = "v2.0.4";
 	public static final String syntaxVersion = "v1.0";
 	public static final String BYNTVersion = "BYNTv1.0";
 	
