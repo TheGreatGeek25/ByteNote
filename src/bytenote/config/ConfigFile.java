33ae8fcc6f8df9f235fe2bddba6aa0d32a31fe53
TheGreatGeek25 <TheGreatGeek25@users.noreply.github.com>
2017-12-28 18:04:57 -0600

    Add config file parsing

--- src/bytenote/config/ConfigFile.java
+++ src/bytenote/config/ConfigFile.java
@@ -1,6 +1,27 @@
 package bytenote.config;
 
 import java.io.File;
+import java.io.IOException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Result;
+import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import bytenote.note.types.NoteTypes.NoteType;
+import javafx.scene.paint.Color;
 
 public class ConfigFile extends File {
 
@@ -13,16 +34,61 @@ public class ConfigFile extends File {
 	}
 	
 	public Config readData() {
-		Config config = new Config();
-		
-		
+		try {
+			Config config = new Config();
+			
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder documentBuilder;
+			documentBuilder = dbFactory.newDocumentBuilder();
+			
+			Document doc = documentBuilder.parse(this);
+			
+			Element configElement = doc.getDocumentElement();
+			config.bytenoteVersion = configElement.getAttribute("bytenoteVersion");
+			
+			Element noteTypes = (Element) doc.getElementsByTagName("noteTypes").item(0);
+			NodeList types = noteTypes.getElementsByTagName("type");
+			for(int i=0; i<types.getLength(); i++) {
+				Element typeNode = (Element) types.item(i);
+				Color color = Color.web(typeNode.getAttribute("color"));
+				config.noteTypes.add( new NoteType(typeNode.getAttribute("name"), color, false) );
+			}
+			
+			return config;
+		} catch (ParserConfigurationException | SAXException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return null;
 	}
 	
-	public void writeData(Config data) {
+	public void writeData(Config data) throws ParserConfigurationException, TransformerException {
 		
+		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder documentBuilder = dbFactory.newDocumentBuilder();
+		Document doc = documentBuilder.newDocument();
 		
+		Element configElement = doc.createElement("config");
+		configElement.setAttribute("bytenoteVersion", data.bytenoteVersion);
+		doc.appendChild(configElement);
 		
+		Element noteTypes = doc.createElement("noteTypes");
+		for(NoteType type: data.noteTypes) {
+			Element typeElement = doc.createElement("type");
+			typeElement.setAttribute("name", type.getName());
+			String red = Integer.toHexString((int) (type.getColor().getRed()*255));
+			String green = Integer.toHexString((int) (type.getColor().getGreen()*255));
+			String blue = Integer.toHexString((int) (type.getColor().getBlue()*255));
+			typeElement.setAttribute("color", "#"+red+green+blue);
+			noteTypes.getParentNode().appendChild(typeElement);
+		}
+		//TODO Finish this
+
+		Transformer transformer = TransformerFactory.newInstance().newTransformer();
+		Result output = new StreamResult(this);
+		Source input = new DOMSource(doc);
+
+		transformer.transform(input, output);
 	}
 	
 	
